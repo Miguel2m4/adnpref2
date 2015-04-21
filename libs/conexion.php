52d1e23b7e871be2a00afcbe01e7df317800ea50
Miguel Gutierrez <miguel.gutierrez@inngeniate.com>
2015-04-21 14:47:23 -0500

    conexion

--- libs/conexion.php
+++ libs/conexion.php
@@ -1,5 +1,5 @@
 <?php
-$conexion=mysql_connect("localhost","hogaresd_inmo15","dd*?EFTg5OM2")or die ("No hay Conexion");
-$conectDB=mysql_select_db("hogaresd_andapref",$conexion) or die ("no existe BD");
+$conexion=mysql_connect("localhost","anda","88021955200")or die ("No hay Conexion");
+$conectDB=mysql_select_db("andaperf2015",$conexion) or die ("no existe BD");
 mysql_query ("SET NAMES 'utf8'");
 ?>
